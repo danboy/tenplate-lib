5cb2c50fb3aae7982de43cb24474c98ecfcafc8e
Dan Nawara <dan@leapinc.co>
2022-08-19 15:32:46 -0500

    wip: initial commit

---
+++ dist/components/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Test = void 0;
+
+var _jsxRuntime = require("react/jsx-runtime");
+
+const Test = () => /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+  children: "test"
+});
+
+exports.Test = Test;
